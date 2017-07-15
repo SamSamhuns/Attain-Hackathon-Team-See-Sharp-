93ef6acf1d116adb565648cfa8e8f57dd62eec33
Fangluz <zhufanglun@hotmail.com>
2017-07-15 15:08:25 -0400

    Add files via upload

--- phaser.py
+++ phaser.py
@@ -1,12 +1,16 @@
 import json
 
-
 def get_ID_by_name(country_name):
     with open('test.json') as data_file:
         data = json.load(data_file)
-        return list(data.keys())[list(data.values()).index('China')]
+        return list(data['id_dic'].keys())[list(data['id_dic'].values()).index(country_name)]
 
 def get_name_by_ID(country_id):
     with open('test.json') as data_file:
         data = json.load(data_file)
-        return data[country_id]
+        return data['id_dic'][country_id]
+
+def get_region(country_id):
+    with open('test.json') as data_file:
+        data = json.load(data_file)
+        return data['region_dic'][country_id]
