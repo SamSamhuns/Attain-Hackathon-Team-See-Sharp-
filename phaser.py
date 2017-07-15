39abb1b87914a700b8f0d2e0541806bbd2ecd234
Fangluz <zhufanglun@hotmail.com>
2017-07-15 15:35:12 -0400

    Update phaser.py

--- phaser.py
+++ phaser.py
@@ -1,17 +1,17 @@
 import json
 
 def get_ID_by_name(country_name):
-    with open('dic.json') as data_file:
+    with open('dict.json') as data_file:
         data = (json.load(data_file))['id_dic']
         return list(data.keys())[list(data.values()).index(country_name)]
 
 def get_name_by_ID(country_id):
-    with open('dic.json') as data_file:
+    with open('dict.json') as data_file:
         data = (json.load(data_file))['id_dic']
         return data[country_id]
 
 def get_region(country_id):
-    with open('dic.json') as data_file:
+    with open('dict.json') as data_file:
         data = (json.load(data_file))['region_dic']
         return data[country_id]
 
@@ -26,7 +26,7 @@ def get_region_by_ID(region_id):
         return data[region_id]
 
 def get_countries(region_id):
-    with open('dic.json') as data_file:
+    with open('dict.json') as data_file:
         data = (json.load(data_file))['region_dic']
         r = []
         for k, v in data.items():
