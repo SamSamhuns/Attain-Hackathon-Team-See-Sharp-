2f40e4000300ddf6e78224f6c4fd01a15b1a627e
SamSamhuns <sthapuskar@yahoo.com>
2019-03-04 00:49:24 +0400

    updated unsued library imports

--- src/svm.py
+++ src/svm.py
@@ -1,7 +1,5 @@
-import csv
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn import svm
 from sklearn.externals import joblib
 #from sklearn import preprocessing
