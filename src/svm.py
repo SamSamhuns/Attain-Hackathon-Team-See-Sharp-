d11b43eb143a3e352cade41265b7ef96be7509e3
SamSamhuns <samhunsadamant@gmail.com>
2020-01-28 13:49:34 +0400

    Create test accuracy func

--- src/svm.py
+++ src/svm.py
@@ -9,17 +9,22 @@ def train(data):
         csvdata = np.loadtxt(data, delimiter=',')
         x = np.delete(csvdata, [4, 5], axis=1)
         y = csvdata[:, 5]
-    except:
-        print('Error reading data')
-    X_train, X_val, y_train, y_val = train_test_split(
+    except Exception as e:
+        print(f'{e}: Error reading data')
+    X_train, X_test, y_train, y_test = train_test_split(
         x, y, test_size=0.33, random_state=1)
 
     clf = svm.SVC(kernel='rbf', random_state=2, C=50, probability=False)
     clf = clf.fit(X_train, y_train)
 
-    y_pred = clf.predict(X_val)
+    y_pred = clf.predict(X_test)
+    print("Accuracy on test data: ", accuracy(y_pred, y_test))
     return clf
 
+def accuracy(y_pred, y_test):
+    if len(y_pred) != len(y_test):
+        raise IndexError("Lengths of y_pred and y_test do not match")
+    return (sum([1 if pred==test else 0 for pred,test in zip(y_pred, y_test)])/len(y_pred)) * 100
 
 def main():
     C = train('data.csv')
